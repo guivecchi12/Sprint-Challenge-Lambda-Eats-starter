87d93e780d948f066d915bf5569dfb00e4ed7dc1
unknown <guivecchi12@gmail.com>
2020-05-19 10:27:02 -0600

    Cypress

--- cypress/integration/pizza_test.js
+++ cypress/integration/pizza_test.js
@@ -5,9 +5,14 @@ describe("Name test", function(){
         cy.get('#name').type("Coder").should("have.value", "Coder");
         cy.get('#size').select('medium').should("have.value", "medium");
         cy.get('#sauce').select('red').should("have.value", "red");
+        cy.get('[name="pepperoni"]').click();
+        cy.get('[name="olives"]').click();
+        cy.get('#inst').type("backdoor").should("have.value", "backdoor");
         cy.get(".submit").click();
 
         cy.get('#name').clear();
+        cy.get('[for="name"] > div > .error').should("be.visible");
+        cy.get('#name').type("Co").should("have.value", "Co");
         cy.get('[for="name"] > div > .error').should("be.visible");
     })
 })
